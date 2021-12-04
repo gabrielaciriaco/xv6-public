f127b4a877a72f561467bbc167124e9fc610f4dd
Gabriela Ciriaco <gabs.ciriaco@gmail.com>
2021-12-03 21:27:27 -0300

    criando primeiro teste

---
+++ test1.c
@@ -0,0 +1,18 @@
+#include "types.h"
+#include "user.h"
+
+int stdout = 1;
+int stderr = 2;
+
+int main(int argc, char *argv[]) {
+  char *music =
+      "Ai, amor será que tu divide a dor do teu peito cansado com alguém que "
+      "não vai te sarar meu amor eu vivo no aguardo de ver você voltando "
+      "cruzando a porta, parararara\n";
+  long int number=0;
+  for(int i=0; i<999999;i++){
+    number +=strlen(music);
+  }
+  printf(stdout,"%d\n",number);
+  exit();
+}
